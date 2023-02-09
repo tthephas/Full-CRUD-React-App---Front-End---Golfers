0bafff35ea94b5cc702fc481023cb1df4be68ea6
thepjos <josh.thephasdin@gmail.com>
2023-02-09 08:18:27 -0600

    fixed an error

--- src/components/shared/LoadingScreen.js
+++ src/components/shared/LoadingScreen.js
@@ -1,10 +1,10 @@
 import Spinner from "react-bootstrap/Spinner";
 
-const LoadingScreen = () => {
+const LoadingScreen = () => (
     <div className="container-sm" style={{ textAlign: 'center'}}>
         <Spinner role="status" animation="border"/>
 
     </div>
-}
+)
 
 export default LoadingScreen
